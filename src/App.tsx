e9875cfd9a6087140df622e866a39a06e252e02d
Lucas dos Santos <72577150+Lucas76512@users.noreply.github.com>
2023-02-05 12:58:06 -0300

    commit change

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,14 @@ import'./Global.css'
 import { Header } from './components/Header'
 import { Post } from "./Post"
 import { Button } from '../src/components/Button/index'
-
-import { Input} from './components/Input/index'
-import { Tasks } from './components/Tasks'
-
-
+import { Task } from './components/Task'
 export function App() {
   return (
      <div>
       <Header />
       <div className='search-wrapper'>
       </div>
-      <Tasks />
+      <Task />
      </div>
   )
 }
